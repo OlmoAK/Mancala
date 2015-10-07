b83bc298d4bde7d65501d6c7d6c55fa67f9c06b4
Tos Alles <talles@sogyo.nl>
2015-10-07 10:35:37 +0200

    Bestandsstructuur opgezet

---
+++ src/main/java/nl/sogyo/mancala/App.java
@@ -0,0 +1,9 @@
+package nl.sogyo.mancala;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/nl/sogyo/mancala/AppTest.java
@@ -0,0 +1,26 @@
+package nl.sogyo.mancala;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void AppTest( String testName ) {
+
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue(true);
+    }
+}
