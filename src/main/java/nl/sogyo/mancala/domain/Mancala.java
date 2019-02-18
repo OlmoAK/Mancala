a554576900948682ad13c080fd062b69b70394d1
rvvugt <rvvugt@sogyo.nl>
2019-02-18 19:23:37 +0100

    Revert to previous version

--- src/main/java/nl/sogyo/mancala/domain/Mancala.java
+++ src/main/java/nl/sogyo/mancala/domain/Mancala.java
@@ -1,44 +1,9 @@
 package nl.sogyo.mancala.domain;
 
-import java.util.Collection;
-
-public interface Mancala {
-
-	/**
-	 * @return
-	 */
-	Pit getPit();
-
-	/**
-	 * @param index
-	 * @return
-	 */
-	Pit getPit(int index);
-
-	/**
-	 * @param index
-	 * @return
-	 */
-	int[] playRecess(int index);
-
-	/**
-	 * @return
-	 */
-	int[] exportGameState();
-
-	/**
-	 * @return
-	 */
-	boolean isEndOfGame();
-
-	/**
-	 * @return
-	 */
-	Collection<Recess> getPlayableRecesses();
-
-	/**
-	 * @return
-	 */
-	Player getWinner();
-
+public class Mancala
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
